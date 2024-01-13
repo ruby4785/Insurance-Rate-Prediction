0d6c0537f081ba9cabea41a7c9f96b592d686e38
Rahul Gowda KK <rahul.gowda4785@gmail.com>
2024-01-13 08:35:05 +0530

    insurance

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,25 +16,19 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
-        '''
-        This function si responsible for data trnasformation
-        
-        '''
         try:
-            numerical_columns = ["writing_score", "reading_score"]
+            numerical_columns = ["age", "bmi","children"]
             categorical_columns = [
-                "gender",
-                "race_ethnicity",
-                "parental_level_of_education",
-                "lunch",
-                "test_preparation_course",
+                "smoker",
+                "sex",
+                "region"
             ]
 
             num_pipeline= Pipeline(
@@ -85,8 +79,8 @@ class DataTransformation:
 
             preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name="math_score"
-            numerical_columns = ["writing_score", "reading_score"]
+            target_column_name="charges"
+            numerical_columns = ["age", "bmi","children"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
